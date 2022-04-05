4954b594c2a0bcdfe5c60530d1b61028667f2212
Alan-Bushell <alanbushell91@gmail.com>
2022-04-05 19:51:26 +0000

    added basic on load script, add data type script

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+let buttons = document.getElementsByTagName("button");
+
+function runGame(gameType){
+    if (gameType === "whoami"){
+        displayWhoAmIQuestion();
+    } else if (gameType === "flag"){
+        displayFlagsQuestion();
+    }
+      else if (gameType === "interestingFact"){
+          displayInterestingFactQuestion
+      } else{
+            alert(`Unknown game type: ${gameType}`);
+            throw `Unknown game type: ${gameType}. End process`;
+        }
+};
+
+function checkAnswers(){
+    /* check answer against stored correct answer */
+};
+
+function countdownClock(){
+/* Time remaining point ststem: 45 >= 100, 30 >= 75, 15 >= 50, 15 < 50 */
+};
+
+function calculateScore(){
+/* Calculate score will involve checking if answer === correct && time remaining to calc score*/
+};
+
+function displayWhoAmIQuestion(){
+
+};
+
+function displayFlagsQuestion(){
+
+};
+
+function displayInterestingFactQuestion(){
+
+};
+
