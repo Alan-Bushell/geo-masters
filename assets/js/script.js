b710a31f0ad4710705eb3b517fd39c376014a8ee
Alan-Bushell <alanbushell91@gmail.com>
2022-04-12 09:47:20 +0000

    add countdown clock with colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,29 @@
 let buttons = document.getElementsByTagName("button");
 
+let counter = 60;
+
+setInterval( function(){
+    counter--;
+
+    if( counter >=30){
+        clock = document.getElementById("clock");
+        clock.innerHTML = counter;
+        clock.style.color = 'limegreen'
+    } else if (counter >=15 && counter < 30){
+        clock.innerHTML = counter;
+        clock.style.color = 'yellow';
+    } else if (counter >=1 && counter < 15){
+        clock.innerHTML = counter;
+        clock.style.color = 'red'
+    } else if (counter === 0){
+        clock.innerHTML = "Times Up";
+        clock.style.color = 'white'
+    }
+}, 1000);
+
+
+
+
 function runGame(gameType){
     if (gameType === "whoami"){
         alert("You have clicked who am i")
@@ -21,6 +45,7 @@ function checkAnswers(){
 
 function countdownClock(){
 /* Time remaining point ststem: 45 >= 100, 30 >= 75, 15 >= 50, 15 < 50 */
+    
 };
 
 function calculateScore(){
