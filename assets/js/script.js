c04b76fb8d696895ec1dceab8a987a245290cef6
Alan-Bushell <alanbushell91@gmail.com>
2022-04-14 14:31:30 +0000

    update html options & add vars to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,7 @@
 let buttons = document.getElementsByTagName("button");
 const question = document.getElementById('question');
-const choices = Array.from(document.getElementByClassName('choices'));
+const choices = Array.from(document.getElementsByClassName('choices'));
 
-let currentQuestion = {};
-let acceptingAnswers = false;
-let score = 0;
-let questionCounter = 0;
-let availableQuestions = []
-
-
-const CORRECT_BONUS = 100;
-const Max_Questions = 8;
-
-startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    getNewQuestion();
-};
-
-getNewQuestion = () => {
-
-    questionCounter ++;
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
-
-    choices.forEach( choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion.['choice' + number];
-    });
-
-    availableQuestions.splice(questionIndex, 1);
-
-    acceptingAnswers = true;
-}
-
-})
 
 // Countdown Timer
 let counter = 60;
