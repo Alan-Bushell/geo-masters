2e2558fd62e03ec2140877d6eabdb5ef427c5707
Alan-Bushell <alanbushell91@gmail.com>
2022-05-01 14:10:13 +0000

    fix broken link

--- assets/js/script.js
+++ assets/js/script.js
@@ -291,7 +291,7 @@ function closeRules(){
 
 function closeEndgame(){
     finalScore.style.display = "none";
-    return window.location.assign("https://alan-bushell.github.io/geo-masters/#")
+    return window.location.reload();
 }
 
 window.onclick = function(event) {
